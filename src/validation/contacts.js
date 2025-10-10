1d51ab12042581b65e6a55816912ff0fd2ca15f6
Yuliia <mykytenko.julia@gmail.com>
2025-10-10 18:45:35 +0300

    validation

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -2,8 +2,16 @@ import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
-  phoneNumber: Joi.number().pattern(/^\+?\d{10,15}$/).required(),
-  email: Joi.string().email(),
+  phoneNumber: Joi.string().pattern(/^\+?\d{10,15}$/).required(),
+  email: Joi.string().email().required(),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal').required(),
 });
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().pattern(/^\+?\d{10,15}$/),
+  email: Joi.string().email(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
+  });
